3273a378da8d4b5bd56453a641aec3639a0dadfe
alberchec <alberchec@hotmail.com>
2021-07-13 04:02:25 -0300

    add conditional_statements.cpp

---
+++ HackerHank/conditional_statements.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    std::string number_names[] = {"one","two","three","four","five","six",
+                                  "seven","eight","nine","Greater than 9"};
+
+    if(n <= 9)
+        std::cout << number_names[n - 1] << std::endl;
+    else
+        std::cout << number_names[9] << std::endl;
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
