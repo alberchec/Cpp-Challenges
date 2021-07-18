df0262af00f2825b65af962ae35895800b8702d4
alberchec <alberchec@hotmail.com>
2021-07-18 01:46:17 -0300

    change files

--- MyOwn/Pipe_multifiles/file1.cpp
+++ MyOwn/Pipe_multifiles/file1.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <fstream>
 using namespace std;
 
 int main(){
-	ifstream input("input.txt");
-	string s;
+	ofstream output_clean_file("inputs.txt");
+	output_clean_file.close();
 
-	while(getline(input,s) ){
-		cout << s << endl;
+	ofstream output("inputs.txt",ios::app);
+
+	int n = 10000;
+	int size = 2;
+
+	cout << n << ' ' << size << endl;
+
+	srand(time(NULL));
+
+	while(n--){
+		int random_cmd = rand() % 2;
+		int random_num = rand() % 5;
+
+		if(random_cmd == 0){
+			cout << "get " << random_num << endl;
+			output << "get " << random_num << endl;
+		}else if(random_cmd == 1){
+			cout << "set " << random_num << " 5\n"; 
+			output << "set " << random_num << " 5\n"; 
+		}
 	}
 
 	return 0;
