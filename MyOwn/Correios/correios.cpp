3275dcdf88a24f1ab90cccd5b1ae2c4d3cea1a07
alberchec <alberchec@hotmail.com>
2021-07-20 09:13:35 -0300

    change correios.cpp

--- MyOwn/Correios/correios.cpp
+++ MyOwn/Correios/correios.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <vector>
-#include <map>
 #include <cstdlib>
 #include <fstream>
 using namespace std;
 
+#define temp_file "/Users/alberto/Documents/Terminal_programs/Correios/track.html"
+#define track_numbers_file "/Users/alberto/Documents/Terminal_programs/Correios/track_numbers.txt"
+
 void parse_correios(string name){
 	string s,parse;
 
-	ifstream input("track.html");
+	ifstream input(temp_file);
 
 	while(getline(input, s) ){
 		if(s.find("<ul class=\"linha_status m-0\" style=\"border-bottom: 0;\"  style=\"\">") != -1){
@@ -58,7 +60,7 @@ void main_program(){
 	string s;
 	vector<vector<string> > v;
 
-	ifstream input("track_numbers.txt");
+	ifstream input(track_numbers_file);
 	if(!input.is_open() ){
 		throw invalid_argument("There aren't available packages for tracking!");
 	}
@@ -86,7 +88,8 @@ void main_program(){
 	
 	//Requesting package status and parsing it
 	for(int i=0;i<v.size();i++){
-		string cmd_string = "curl https://www.linkcorreios.com.br/ -s > track.html";
+		string cmd_string = "curl https://www.linkcorreios.com.br/ -s > ";
+		cmd_string += temp_file;
 		cmd_string.insert(37,v[i][0]);
 		const char* cmd = cmd_string.c_str();
 		system(cmd);
@@ -97,7 +100,7 @@ void main_program(){
 void list_track_numbers(){
 	string s;
 
-	ifstream input("track_numbers.txt");
+	ifstream input(track_numbers_file);
 	if(!input.is_open() ){
 		throw invalid_argument("track_numbers file empty!");
 	}
@@ -111,13 +114,13 @@ void list_track_numbers(){
 }
 
 void add_track_number(string arg,string arg2){
-	ofstream output("track_numbers.txt", ios::app);
+	ofstream output(track_numbers_file, ios::app);
 	output << arg << " - " << arg2 << endl;
 }
 
 void delete_track_number(int num=-1){
 	string s,file_content="";
-	ifstream input("track_numbers.txt");
+	ifstream input(track_numbers_file);
 	if(!input.is_open() ){
 		throw invalid_argument("track_numbers file empty!");
 	}
@@ -130,35 +133,27 @@ void delete_track_number(int num=-1){
 		num--;
 	}
 	jump:
-	ofstream output("track_numbers.txt");
+	ofstream output(track_numbers_file);
 	output << file_content;
 
 }
 
 int main(int argc, char* argv[]){
 	try{
-		if(argc == 1){
+		string arg = argv[1];
+		if(arg == ""){
 			main_program();
-		}else if(argc == 2){
-			string arg = argv[1];
-			if(arg == "-l"){
-				list_track_numbers();
-			}else if(arg == "-d"){
-				delete_track_number();
-			}
-		}else if(argc == 3){
-			string arg = argv[1];
-			if(arg == "-d"){
-				unsigned int arg2 = atoi(argv[2]);
-				delete_track_number(arg2);
-			}else{
-				string arg2 = argv[2];
-				add_track_number(arg,arg2);
-			}
+		}else if(arg == "-l"){
+			list_track_numbers();
+		}else if(arg == "-d"){
+			unsigned int arg2 = atoi(argv[2]);
+			delete_track_number(arg2);
+		}else if(arg == "--d-all"){
+			delete_track_number();	
 		}else{
-			throw invalid_argument("Invalid argument(s)!");
+			string arg2 = argv[2];
+			add_track_number(arg,arg2);
 		}
-
 	}catch(exception& e){
 		cout << "Exception: " << e.what() << endl;
 	}
