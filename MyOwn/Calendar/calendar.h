8b61ab4b001d8ad43acdbe1c5fd28ed8aa1a56e5
alberchec <alberchec@hotmail.com>
2021-08-09 00:37:42 -0300

    fixing issues

--- MyOwn/Calendar/calendar.h
+++ MyOwn/Calendar/calendar.h
@@ -5,7 +5,7 @@ class Calendar{
 private:
 	static const int start_year = 2020; //Leap year
 	static const int start_weekday = 4; //Wednesday
-	static const int months_length[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+	static constexpr int months_length[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 	int day;
 	int month;
 	int year;
