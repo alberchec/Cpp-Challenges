1bfaa75283be444a73be277fd5555bec455862e2
alberchec <alberchec@hotmail.com>
2021-08-09 01:26:34 -0300

    changes calendar

--- MyOwn/Calendar/calendar.cpp
+++ MyOwn/Calendar/calendar.cpp
@@ -1,11 +1,9 @@
-#include "calendar.h"
-
 Calendar::Calendar(int d, int m, int y) : day(d), month(m), year(y){
-	absolute_date();
+	abs_date();
 	set_month_length();
 }
 
-void Calendar::absolute_date(){
+void Calendar::abs_date(){
 	absolute_date = 0;
 	int year_diff = year - start_year;
 	if(year_diff) absolute_date = 1;
@@ -19,13 +17,21 @@ void Calendar::absolute_date(){
 }
 
 void Calendar::set_month_length(){
+	bool leap = leap_year();
+
 	if(month != 2){
 		month_length = months_length[month - 1];
-	}else if(leap_year() ){
+	}else if(leap){
 		month_length = 29;
 	}else{
 		month_length = 28;
 	}
+
+	if(month == 1)    prev_month_length = 31;
+	else if(month == 3){
+		if(leap) prev_month_length = 29;
+		else		  prev_month_length = 28;
+	}else		      prev_month_length = months_length[month-2];
 
 }
 
@@ -41,20 +47,18 @@ bool Calendar::leap_year(){
 }
 
 int Calendar::get_weekday(){
-	int year_diff = year - start_year;
-	int day_diff = 0;
-	if(year_diff) day_diff = 1;
-	day_diff = (year_diff - 1) / 4 + 365 * year_diff;
-
-	for(int i=0;i<month-1;i++){
-		day_diff += month_length;
-	}
-	day_diff += day - 1;
-
-	int weekday = (day_diff + start_weekday) % 7;
+	int weekday = (absolute_date + start_weekday) % 7;
 	return weekday;
 }
 
 int Calendar::get_month_length(){
 	return month_length;
+}
+
+int Calendar::get_abs_date(){
+	return absolute_date;
+}
+
+int Calendar::get_prev_m_len(){
+	return prev_month_length;
 }
--- MyOwn/Calendar/calendar.h
+++ MyOwn/Calendar/calendar.h
@@ -4,7 +4,7 @@
 class Calendar{
 private:
 	static const int start_year = 2020; //Leap year
-	static const int start_weekday = 4; //Wednesday
+	static const int start_weekday = 3; //Wednesday
 	static constexpr int months_length[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 	int day;
 	int month;
@@ -12,13 +12,15 @@ private:
 	int absolute_date;
 	int month_length;
 	int prev_month_length;
-	void absolute_date();
+	void abs_date();
 	void set_month_length();
 public:
 	Calendar(int,int,int);
 	bool leap_year(); //Conditions to occur a leap year
 	int get_weekday();
 	int get_month_length();
+	int get_abs_date();
+	int get_prev_m_len();
 };
 
 #endif
