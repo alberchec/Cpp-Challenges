cd1f906deb0c458fc3915c4515030659ddbaa880
alberchec <alberchec@hotmail.com>
2021-08-09 20:03:01 -0300

    fixing issues Format_string

--- MyOwn/Format_string/format_string.h
+++ MyOwn/Format_string/format_string.h
@@ -3,9 +3,17 @@
 
 #include <iostream>
 
-void format_string(std::string&,std::string);
+void format_string(std::string& s, std::string first){
+	int pos = s.find("{}");
+	if(pos != -1){
+		s.replace(pos,2,first);
+	}
+}
 
 template<typename...strs>
-void format_string(std::string&,std::string,strs...);
+void format_string(std::string& s, std::string first, strs...rest){
+	format_string(s,first);
+	format_string(s,rest...);
+}
 
 #endif
