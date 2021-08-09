31c56ba733f8958c1465aa320159953cdb838162
alberchec <alberchec@hotmail.com>
2021-08-08 21:28:22 -0300

    changes main.cpp

--- MyOwn/Calendar/main.cpp
+++ MyOwn/Calendar/main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <fstream>
 #include "calendar.h"
+
 using namespace std;
 
 int main(){
 	Calendar cal;
-	cout << cal.get_weekday(1,7,2067) << endl;
+	cout << "\nThis program creates a month calendar!\n\n";
+
+	cout << "Enter the desired month number: ";
+	int month;
+	std::string month_name[12] = {"Janeiro","Fevereiro","Marco","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+	cin >> month;
+
+	cout << "Enter the desired year: ";
+	int year;
+	cin >> year;
+
+	ifstream input("calendar.html");
+	if(!input.is_open()){
+		cout << "Calendar.html not found!\n";
+		return 1;
+	}
+
+	string calendar_html = "",temp;
+	while(getline(input,temp) ){
+		calendar_html += temp;
+	}
+
+	input.close();
+
+	temp = to_string(month) + "-" + to_string(year) + ".html";
+	ofstream output(temp,ios::app);
+
+	int pos = calendar_html.find("/--Replace_with_month--/");
+	calendar_html.replace(pos,24,month_name[month-1]);
+
+	pos = calendar_html.find("/--Replace_with_year--/");
+	calendar_html.replace(pos,23,year);
+
+	output << calendar_html;
+	output.close();
 }
