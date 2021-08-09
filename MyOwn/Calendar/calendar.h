4c1d7e3c8f22412cf9ac789dda12f090d470d8f1
alberchec <alberchec@hotmail.com>
2021-08-09 00:20:11 -0300

    remove parameters

--- MyOwn/Calendar/calendar.h
+++ MyOwn/Calendar/calendar.h
@@ -9,11 +9,11 @@ private:
 	const int start_year = 2020; //Leap year
 	const int start_weekday = 4; //Wednesday
 	int months_length[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-	void set_month_length(int);
+	void set_month_length();
 public:
 	Calendar(int d,int m,int y) : day(d), month(m), year(y){}
-	bool leap_year(int); //Conditions to occur a leap year
-	int get_weekday(int,int,int);
+	bool leap_year(); //Conditions to occur a leap year
+	int get_weekday();
 	//int get_month_length(int,int);
 };
 
